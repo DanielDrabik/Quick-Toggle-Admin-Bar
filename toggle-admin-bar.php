16603ecaf77ca7770f728f58ea6f11fa7c528392
Daniel Drabik <daniel.drabik94@gmail.com>
2018-07-20 01:31:06 +0200

    Updated license info

--- toggle-admin-bar.php
+++ toggle-admin-bar.php
@@ -5,31 +5,11 @@ Plugin Name: Toggle Admin Bar
 Plugin URI: https://github.com/DanielDrabik/Toggle-Admin-Bar/
 Description: Allow users to toggle (hide and show) admin menu bar.
 Version: 1.0
+License:     GPL3
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
 Author: Daniel Drabik
 Author URI: https://github.com/DanielDrabik/
 */
-
-/**
- * Copyright (c) 2018 . All rights reserved.
- *
- * Released under the GPL license
- * http://www.opensource.org/licenses/gpl-license.php
- *
- * This is an add-on for WordPress
- * http://wordpress.org/
- *
- * **********************************************************************
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * **********************************************************************
- */
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
